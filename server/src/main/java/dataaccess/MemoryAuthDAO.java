afb132c15bbad3eac0c777141bda4afb38c671c4
Alexander Evans <alexander.evans@byu.edu>
2024-10-22 17:12:06 -0600

    create MemoryAuthDAO from interface

---
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -0,0 +1,49 @@
+package dataaccess;
+import model.AuthData;
+import java.util.HashMap;
+import java.util.Map;
+
+public class MemoryAuthDAO implements AuthDAO {
+
+    private final Map<String, AuthData> authTokens = new HashMap<>();
+    @Override
+    public AuthData findAuthByToken(String authToken) {
+        return authTokens.get(authToken);
+    }
+    @Override
+    public void clear() {
+        authTokens.clear();
+    }
+
+    @Override
+    public boolean validateAuthToken(String authToken) {
+        return authTokens.containsKey(authToken);
+    }
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+        if (authTokens.containsKey(auth.authToken())) {
+            throw new DataAccessException("Auth token already exists: " + auth.authToken());
+        }
+        authTokens.put(auth.authToken(), auth);
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        AuthData authData = authTokens.get(authToken);
+        if (authData == null) {
+            throw new DataAccessException("Auth token not found: " + authToken);
+        }
+        return authData;
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+        if (!authTokens.containsKey(authToken)) {
+            throw new DataAccessException("Auth token not found: " + authToken);
+        }
+        authTokens.remove(authToken);
+    }
+}
+
+

ec6f404dcb2862248f74240492ab2a227bac1510
Alexander Evans <alexander.evans@byu.edu>
2024-10-22 17:06:33 -0600

    finish AuthDAO interface

--- server/src/main/java/dataaccess/AuthDAO.java
+++ server/src/main/java/dataaccess/AuthDAO.java
@@ -1,4 +1,12 @@
 package dataaccess;
 
+import model.AuthData;
+
 public interface AuthDAO {
+    void createAuth(AuthData auth) throws DataAccessException;
+    AuthData getAuth(String authToken) throws DataAccessException;
+    void deleteAuth(String authToken) throws DataAccessException;
+    void clear() throws DataAccessException;
+    boolean validateAuthToken(String authToken) throws DataAccessException;
+    AuthData findAuthByToken(String authToken) throws DataAccessException;
 }

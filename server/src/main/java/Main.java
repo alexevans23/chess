f70799c87d19d29f30f011605efded6bad1031b5
Alexander Evans <alexander.evans@byu.edu>
2024-10-22 12:13:59 -0600

    added server instance to main to check frontend

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,9 +1,13 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
-
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Server: " + piece);
+        int port = 8080;
+        if (args.length >= 1) {
+            port = Integer.parseInt(args[0]);
+        }
+        new Server().run(port);
+        System.out.printf("Server started on port %d%n", port);
     }
 }

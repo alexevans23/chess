0264d6f58aa10bc5759b9ffc90bbf3396b4e61c3
Alexander Evans <alexander.evans@byu.edu>
2024-10-31 15:23:39 -0600

    implement stubbed out MySQLAUthDAO from AuthDAO interface

---
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -0,0 +1,34 @@
+package dataaccess;
+
+import model.AuthData;
+
+public class MySQLAuthDAO implements AuthDAO {
+
+    @Override
+    public void createAuth(AuthData auth) throws DataAccessException {
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null; // Placeholder return value
+    }
+
+    @Override
+    public void deleteAuth(String authToken) throws DataAccessException {
+    }
+
+    @Override
+    public void clear() throws DataAccessException {
+    }
+
+    @Override
+    public boolean validateAuthToken(String authToken) throws DataAccessException {
+        return false;
+    }
+
+    @Override
+    public AuthData findAuthByToken(String authToken) throws DataAccessException {
+        return null;
+    }
+}
+

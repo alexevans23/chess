d5845fe27b241040ea807b2251a0477b8403900c
Alexander Evans <alexander.evans@byu.edu>
2024-10-31 19:09:59 -0600

    implement clear method and execute update method for executing SQL

--- server/src/main/java/dataaccess/MySQLUserDAO.java
+++ server/src/main/java/dataaccess/MySQLUserDAO.java
@@ -1,7 +1,9 @@
 package dataaccess;
-
 import model.UserData;
 import java.sql.*;
+import com.google.gson.Gson;
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+import static java.sql.Types.NULL;
 
 public class MySQLUserDAO implements UserDAO {
     public MySQLUserDAO() throws DataAccessException {
@@ -9,6 +11,8 @@ public class MySQLUserDAO implements UserDAO {
     }
     @Override
     public void clear() throws DataAccessException {
+        String statement = "DELETE FROM users";
+        executeUpdate(statement);
     }
 
     @Override
@@ -24,6 +28,25 @@ public class MySQLUserDAO implements UserDAO {
             System.out.println("Database connected successfully.");
         } catch (SQLException e) {
             throw new DataAccessException("Failed to connect to database: " + e.getMessage());
+        }
+    }
+    private void executeUpdate(String statement, Object... params) throws DataAccessException {
+        try (Connection conn = DatabaseManager.getConnection();
+             PreparedStatement ps = conn.prepareStatement(statement)) {
+
+            for (int i = 0; i < params.length; i++) {
+                Object param = params[i];
+                switch (param) {
+                    case String p -> ps.setString(i + 1, p);
+                    case Integer p -> ps.setInt(i + 1, p);
+                    case null -> ps.setNull(i + 1, NULL);
+                    default -> {
+                    }
+                }
+            }
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("Unable to update database: " + e.getMessage());
         }
     }
 }

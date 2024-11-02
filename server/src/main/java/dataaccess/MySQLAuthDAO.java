e64798a5bfb2273fa54f401c6fc5469b2d47bb2e
Alexander Evans <alexander.evans@byu.edu>
2024-11-01 18:38:21 -0600

    Implement configureDatabase and createStatements similar to MySQLUserDAO

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -5,9 +5,13 @@ import java.sql.*;
 import static java.sql.Types.NULL;
 
 public class MySQLAuthDAO implements AuthDAO {
+    public MySQLAuthDAO() throws DataAccessException {
+        configureDatabase();
+    }
 
     @Override
     public void createAuth(AuthData auth) throws DataAccessException {
+
     }
 
     @Override
@@ -49,6 +53,32 @@ public class MySQLAuthDAO implements AuthDAO {
             ps.executeUpdate();
         } catch (SQLException e) {
             throw new DataAccessException("Unable to update database: " + e.getMessage());
+        }
+    }
+    private final String[] createStatements = {
+            """
+            CREATE TABLE IF NOT EXISTS auth (
+                authToken VARCHAR(50) PRIMARY KEY,
+                username VARCHAR(50) NOT NULL
+            )
+            """
+    };
+    public void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (Connection conn = DatabaseManager.getConnection()) {
+            System.out.println("Connected to database for configuration.");
+            for (String statement : createStatements) {
+                System.out.println("Executing statement: " + statement);
+                try (PreparedStatement preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                    System.out.println("Table created or already exists.");
+                } catch (SQLException ex) {
+                    System.err.println("Error executing statement: " + statement);
+                    throw new DataAccessException("Unable to configure database: " + ex.getMessage());
+                }
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("Unable to configure database: " + ex.getMessage());
         }
     }
 }

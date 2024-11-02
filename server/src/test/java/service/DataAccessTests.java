aaa105ac999a5ae920745cd79633ac701cf7ffe5
Alexander Evans <alexander.evans@byu.edu>
2024-11-01 19:26:05 -0600

    JUNIT tests for getAuth, createAuth, validateAUth, deleteAuth in DataAccessTests

--- server/src/test/java/service/DataAccessTests.java
+++ server/src/test/java/service/DataAccessTests.java
@@ -1,23 +1,31 @@
 package service;
 
 import dataaccess.MySQLUserDAO;
+import dataaccess.MySQLAuthDAO;
 import dataaccess.DataAccessException;
 import model.UserData;
+import model.AuthData;
 import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class DataAccessTests {
     private MySQLUserDAO userDAO;
+    private MySQLAuthDAO authDAO;
     @BeforeEach
     public void setUp() throws DataAccessException {
         userDAO = new MySQLUserDAO();
         userDAO.configureDatabase();
         userDAO.clear();
+
+        authDAO = new MySQLAuthDAO();
+        authDAO.configureDatabase();
+        authDAO.clear();
     }
     @AfterEach
     public void tearDown() throws DataAccessException {
         userDAO.clear();
+        authDAO.clear();
     }
     @Test
     public void testCreateUserSuccess() {
@@ -64,7 +72,62 @@ public class DataAccessTests {
     public void testGetUserNotFound() {
         assertThrows(DataAccessException.class, () -> userDAO.getUser("non_existent_user"));
     }
+    @Test
+    public void testCreateAuthSuccess() {
+        AuthData newAuth = new AuthData("authToken123", "test_user");
+
+        assertDoesNotThrow(() -> {
+            authDAO.createAuth(newAuth);
+            System.out.println("Auth token created successfully!");
+        });
+    }
+    @Test
+    public void testCreateAuthDuplicate() {
+        AuthData newAuth = new AuthData("duplicateToken", "test_user");
+
+        assertDoesNotThrow(() -> authDAO.createAuth(newAuth));
+        assertThrows(DataAccessException.class, () -> authDAO.createAuth(newAuth));
+    }
+    @Test
+    public void testGetAuthSuccess() {
+        AuthData newAuth = new AuthData("authToken123", "test_user");
+
+        assertDoesNotThrow(() -> {
+            authDAO.createAuth(newAuth);
+            AuthData retrievedAuth = authDAO.getAuth("authToken123");
+            assertNotNull(retrievedAuth);
+            assertEquals("authToken123", retrievedAuth.authToken());
+            assertEquals("test_user", retrievedAuth.username());
+            System.out.println("Auth token retrieved successfully!");
+        });
+    }
+    @Test
+    public void testGetAuthNotFound() {
+        assertThrows(DataAccessException.class, () -> authDAO.getAuth("non_existent_token"));
+    }
+    @Test
+    public void testValidateAuthTokenSuccess() throws DataAccessException {
+        AuthData newAuth = new AuthData("validToken", "test_user");
+
+        assertDoesNotThrow(() -> authDAO.createAuth(newAuth));
+        assertTrue(authDAO.validateAuthToken("validToken"));
+    }
+    @Test
+    public void testValidateAuthTokenFailure() throws DataAccessException {
+        assertFalse(authDAO.validateAuthToken("invalidToken"));
+    }
+    @Test
+    public void testDeleteAuthSuccess() {
+        AuthData newAuth = new AuthData("tokenToDelete", "test_user");
+
+        assertDoesNotThrow(() -> {
+            authDAO.createAuth(newAuth);
+            authDAO.deleteAuth("tokenToDelete");
+            assertThrows(DataAccessException.class, () -> authDAO.getAuth("tokenToDelete"));
+        });
+    }
 }
+
 
 
 
